11b538b7c485b681bb2be88eb448d038d02ffe2b
YevhenSu <susidka.yevhen@gmail.com>
2023-10-11 17:06:46 +0300

    routes were removes from refactored index.js file 	modified:   index.js

--- index.js
+++ index.js
@@ -16,24 +16,6 @@ app.use(cors())
 app.use(express.json())
 app.use( '/api/blogs', blogsRouter )
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
