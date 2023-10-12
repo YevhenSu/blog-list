4631d2a50207a9f8827848040eff2e36dc4d410e
YevhenSu <susidka.yevhen@gmail.com>
2023-10-12 15:23:15 +0300

    function totalLikes was moved into list_helper.js file
    	modified:   utils/list_helper.js
    	deleted:    utils/totalLikes.js

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = ( blogs ) => {
+  const likes = blogs.map( blog => blog.likes )
+  return likes.reduce( ( acc, val ) => acc + val, 0 )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
--- utils/totalLikes.js
+++
@@ -1,9 +0,0 @@
-const totalLikes = ( blogs ) => {
-  const likes = blogs.map( blog => blog.likes )
-  return likes.reduce( ( acc, val ) => acc + val, 0 )
-}
-
-
-module.exports = {
-  totalLikes
-}
